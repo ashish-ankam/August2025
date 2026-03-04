ccd43a9741804ae8fd408496d8469f9db00847cd
ashish-ankam <ashishankam1999@gmail.com>
2026-03-04 21:01:30 +0530

    custom exception handling class

--- src/customexcception/DrivingLicenceApp.java
+++ src/customexcception/DrivingLicenceApp.java
@@ -1,5 +1,28 @@
 package customexcception;
 
-public class DrivingLicenceApp {
+import java.util.Scanner;
 
+public class DrivingLicenceApp {
+	String name;
+	int age;
+	public void TakeInput() {
+		System.out.println("Welcome to Driving Licence App");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("enter the name ");
+			 name = sc.next();
+			System.out.println("enter the age ");
+			age = sc.nextInt();
+		}
+		
+		}
+
+	public void Validate() throws OverAgeException {
+		if(age<=70) {
+			System.out.println("Valid age proceed further");
+			}
+		else {
+			OverAgeException oe = new OverAgeException("Please Enter the age correctly less than 70");
+			throw oe; 
+		}
+	}
 }
---
+++ src/customexcception/OverAgeException.java
@@ -0,0 +1,7 @@
+package customexcception;
+
+public class OverAgeException extends Exception {
+    OverAgeException(String msg){
+    	super(msg);
+    }
+}
